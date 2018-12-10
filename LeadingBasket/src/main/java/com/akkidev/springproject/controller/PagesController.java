e3a2d01807e2c6128a8746f86d8b0d7f0d85e1a9
Aakash <Aakash@Aakash>
2018-12-10 20:05:53 +0530

    Add new web template

--- LeadingBasket/src/main/java/com/akkidev/springproject/controller/PagesController.java
+++ LeadingBasket/src/main/java/com/akkidev/springproject/controller/PagesController.java
@@ -1,15 +1,23 @@
 package com.akkidev.springproject.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PagesController {
 
+	private static final Logger logger = LoggerFactory.getLogger(PagesController.class);
+
 	@RequestMapping("/")
-	public String handler(Model model) {
-		model.addAttribute("msg", "LeadingBasket");
+	public String pagesHandler(ModelAndView model) throws Exception {
+
 		return "index";
 	}
 }
